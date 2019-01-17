64dcb6b15bfb9f208a66f1bf060c634b7e5acaea
Marc Dos Santos <mdossantosdev@gmail.com>
2019-01-17 17:03:55 +0100

    Day 6 - bikeshop part2 done

--- bikeshop/routes/index.js
+++ bikeshop/routes/index.js
@@ -10,17 +10,42 @@ const dataBike = [
   { name: 'Model AMIG39', price: 599, url: '/images/bike-6.jpg' },
 ];
 
-const shoppingCart = [
-  { name: 'Model BIKO45', price: 679, url: '/images/bike-1.jpg', quantity: 1 },
-  { name: 'Model ZOOK7', price: 799, url: '/images/bike-2.jpg', quantity: 1 },
-];
+const shoppingCart = [];
 
+/* GET */
 router.get('/', (req, res) => {
   res.render('home', { dataBike });
 });
 
 router.get('/cart', (req, res) => {
   res.render('cart', { shoppingCart });
+});
+
+/* POST */
+router.post('/add-bike', (req, res) => {
+
+  shoppingCart.push({
+    name: req.body.bikeName,
+    price: req.body.bikePrice,
+    url: req.body.bikeImage,
+    quantity: req.body.bikeQuantity,
+  });
+
+  res.redirect('/cart');
+});
+
+router.post('/delete-bike', (req, res) => {
+  let position = req.body.position;
+
+  shoppingCart.splice(position, 1);
+
+  res.redirect('/cart');
+});
+
+router.post('/update-product', (req, res) => {
+  shoppingCart[req.body.position].quantity = req.body.quantity;
+
+  res.redirect('/cart');
 });
 
 module.exports = router;
