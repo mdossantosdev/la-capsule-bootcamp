99a001f8a76994c6b0842997586a87ec591543e3
Marc Dos Santos <mdossantosdev@gmail.com>
2019-02-21 16:18:39 +0100

    Day 33 - locapic part4 done

--- locapic/app/screens/HomeScreen.js
+++ locapic/app/screens/HomeScreen.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements';
+import { ImageBackground, View, StyleSheet } from 'react-native';
+import { Avatar, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { AuthSession } from 'expo';
 import { API_URL } from '../config';
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount = () => {
+    AsyncStorage.getItem('user', (err, data) => {
+      const userJSON = JSON.parse(data);
+      this.props.signin(userJSON);
+    });
+  };
 
   _handlePressAsync = async () => {
     const redirectUrl = AuthSession.getRedirectUrl();
@@ -14,6 +24,8 @@ class HomeScreen extends Component {
       authUrl: `${API_URL}/auth/facebook?redirectUrl=${redirectUrl}`,
     });
 
+    const userString = JSON.stringify(result.params);
+    AsyncStorage.setItem('user', userString);
     this.props.signin(result.params);
 
     if (result.type == 'success') {
@@ -27,7 +39,17 @@ class HomeScreen extends Component {
         source={require('../assets/background.jpg')}
         style={styles.container}
       >
-        <Button title='Sign In' onPress={this._handlePressAsync} />
+        {this.props.user ? (
+          <View style={styles.container}>
+            <Avatar rounded source={{ uri: this.props.user.picture }} />
+            <Button
+              title={`Welcome back ${this.props.user.firstName}`}
+              onPress={() => this.props.navigation.navigate('Map')}
+            />
+          </View>
+        ) : (
+          <Button title='Sign In' onPress={this._handlePressAsync} />
+        )}
       </ImageBackground>
     );
   }
@@ -38,9 +60,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ffffff',
   },
 });
+
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -54,6 +81,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(HomeScreen);
