58cdeea6aca8834ec2ed3b58a4422f7397ffe69c
Marc Dos Santos <mdossantosdev@gmail.com>
2019-03-25 13:16:10 +0100

    Final exam - Add Jumbotron component

--- mytechworld/app/src/components/Home.js
+++ mytechworld/app/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import Jumbotron from './Jumbotron';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Jumbotron />
       </div>
     )
   }
---
+++ mytechworld/app/src/components/Jumbotron.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Jumbotron, Container, Button } from 'reactstrap';
+
+const HeroImage = (props) => {
+  return (
+    <div>
+      <Jumbotron style={styles.hero} fluid>
+        <Container fluid>
+          <h1 className='display-3' style={{ fontWeight: 600 }}>
+            My Tech World
+          </h1>
+          <p className='lead'>10 weeks to change my life</p>
+          <br />
+          <p>8 Fullstack projects to learn how to code</p>
+          <p className='lead'>
+            <Button color='dark'>Discover my projects</Button>
+          </p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default HeroImage;
+
+const styles = {
+  hero: {
+    height: '60vh',
+    backgroundColor: 'grey',
+    backgroundImage: 'url(./jumbotron.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    textAlign: 'center',
+    color: 'rgb(255, 255, 255)',
+  },
+};
