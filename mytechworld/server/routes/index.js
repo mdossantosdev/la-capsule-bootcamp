828f374f61291d79dbb41e3b1c8f144ca45bb2ca
Marc Dos Santos <mdossantosdev@gmail.com>
2019-03-25 10:23:41 +0100

    Final exam - Add DATA.json and create /projects route

--- mytechworld/server/routes/index.js
+++ mytechworld/server/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const projectModel = require('../models/project');
+const DATA = require('../data/data.json');
 
 /* GET */
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/projects', (req, res) => {
+  const projects = DATA.projects;
+  res.json({ result: true, projects: projects });
 });
 
 module.exports = router;
